1cab1b2d75f25566a4cafa4229ce5a55579277c6
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-08 12:30:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Пользователь вводит число месяца.
+Вывести для этого дня: выходной / рабочий день.
+*/
+
+using static System.Console;
+
+WriteTitle("Рабочий/нерабочий день");
+
+bool repeat;
+do
+{
+    WorkDay();
+    WriteLine();
+    Write("Хотите повторить? (д/н): ");
+    string answer = ReadLine();
+    repeat = answer.Trim().ToLower() == "д";
+    WriteLine();
+} while(repeat);
+
+ExitApp();
+
+void WorkDay()
+{
+    int dayNumber = EnterInteger("Введите номер дня месяца (1-28): ");
+    int remainder = dayNumber % 7;
+    if (remainder == 0 || remainder == 1)
+        WriteLine("Выходной день");
+    else
+        WriteLine("Рабочий день");
+}
+
+int EnterInteger(string prompt)
+{
+    Write(prompt);
+    bool result = int.TryParse(ReadLine(), out int number);
+    while(!result)
+    {
+        Write($"Некорректный ввод. {prompt}");
+        result = int.TryParse(ReadLine(), out number);
+    }
+    return number;
+}
+
+void WriteTitle(string title)
+{
+    WriteLine(title);
+    WriteLine(new string('-', title.Length));
+    WriteLine();
+}
+
+void ExitApp()
+{
+    WriteLine();
+    Write("Для выхода нажми Enter");
+    ReadLine();
+}
